a930253c10d462ec6d4e504637f93b8f80b58834
theatypicaldeveloper <mighty.matty91@gmail.com>
2022-09-24 18:39:46 +0100

    Initial commit

---
+++ Variables/namingVariables.js
@@ -0,0 +1,16 @@
+// WRITE CLEAN CODE #1 Variables
+
+// Naming variables
+
+// DON'T
+const random = 'example@gmail.com';
+const arr = ['Volvo', 'BMW', 'Mercedes'];
+
+// DO
+const email = 'example@gmail.com';
+const cars = ['Volvo', 'BMW', 'Mercedes'];
+
+// General rules
+// use camelCase for identifier names (vars and functions)
+// all names should start with a letter
+// hardcoded vars should be capitalized
